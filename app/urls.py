c5e795c0b18517d9d38499d3bb701a70e3f2b243
vincentmuya <vincentmuya13@gmail.com>
2018-11-02 00:43:03 +0300

    created basic layout of track template

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,6 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^cargo/', views.cargo_list, name = 'cargo'),
     url(r'^new/cargo$', views.new_cargo, name='new-cargo'),
-    url(r'^track',views.track_cargo, name='track_cargo'),
+    url(r'^track',views.track_cargo, name='track-cargo'),
 
     ]
